3f5989392799ac9e788c683292e9754d8b73e971
NurdPlay <142042400+NurdPlay@users.noreply.github.com>
2023-12-09 11:12:13 -0800

    Passport JWT

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
-const session = require("express-session")
-const FileStore = require("session-file-store")(session)
 const passport = require('passport')
-const authenticate = require('./authenticate')
+const config = require('./config')
 
-const url = "mongodb://localhost:27017/nucampsite";
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
@@ -37,38 +34,15 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser(12345 - 54321 - 21985 - 21985));
-app.use(session({
-  name: "session-id",
-  secret: "12345-54321-21985-21985",
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}))
+
+
 
 app.use(passport.initialize());
-app.use(passport.session());
+
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.user)
-
-  if (!req.user) {
-      const err = new Error("You are not authenticated");
-      err.status = 401;
-      return next(err);
-    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    const user = auth[0];
-    const pass = auth[1];
-  } else {
-      return next();
-  }
-}
-app.use(auth);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/campsites", campsiteRouter);
